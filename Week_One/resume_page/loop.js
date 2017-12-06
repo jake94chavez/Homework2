1cf025a5762d96a0a053080d79daff98f1029b59
Jake Chavez <jake94chavez@gmail.com>
2017-12-06 08:54:16 -0600

    Commit 9 - Created loop.js file

---
+++ Week_One/resume_page/loop.js
@@ -0,0 +1,33 @@
+for (var i = 0; i <= 99; i++) {
+	console.log(i)
+}
+
+for (var i =99; i >= 0; i--) {
+	console.log(i)
+}
+
+for (var i=0; i <= 98; i++) {
+	if(i%2 === 0) {
+		console.log(i)
+	}
+}
+
+for (var i=99; i >= 0; i--) {
+	if(i%2 === 0) {
+		console.log(i)
+	}
+}
+
+for (var i = 49; i <= 72; i++) {
+	console.log(i)
+}
+
+for (var i = 81; i >= 26; i--) {
+	console.log(i)
+}
+
+for (var i=3; i <= 99; i++) {
+	if(i%3 === 0) {
+		console.log(i)
+	}
+}
